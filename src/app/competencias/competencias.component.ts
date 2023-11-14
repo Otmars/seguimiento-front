881f346af4c1c4fb27ac2b86adbbc91bbff21382
DESKTOP-R6DHVIO\Otmars <ootmar1998@gmail.com>
2023-11-14 11:00:43 -0400

    iniciando graficos cambiando iconos

--- src/app/competencias/competencias.component.ts
+++ src/app/competencias/competencias.component.ts
@@ -17,9 +17,36 @@ import { Table } from 'primeng/table';
   providers: [MessageService, ConfirmationService],
 })
 export class CompetenciasComponent implements OnInit {
+  data: any;
+
+  options: any;
+
 modalGraficos() {
   this.dialogGraficos = true
-  
+  const documentStyle = getComputedStyle(document.documentElement);
+        const textColor = documentStyle.getPropertyValue('--text-color');
+
+        this.data = {
+            labels: ['A', 'B', 'C'],
+            datasets: [
+                {
+                    data: [540, 325, 702],
+                    backgroundColor: [documentStyle.getPropertyValue('--blue-500'), documentStyle.getPropertyValue('--yellow-500'), documentStyle.getPropertyValue('--green-500')],
+                    hoverBackgroundColor: [documentStyle.getPropertyValue('--blue-400'), documentStyle.getPropertyValue('--yellow-400'), documentStyle.getPropertyValue('--green-400')]
+                }
+            ]
+        };
+
+        this.options = {
+            plugins: {
+                legend: {
+                    labels: {
+                        usePointStyle: true,
+                        color: textColor
+                    }
+                }
+            }
+        };
 }
   tipoCompetencia = [
     { nombre: 'Macro Competencia' },
