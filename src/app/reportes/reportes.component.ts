c0e7c018bdf7ade395225daabde5743de0627655
DESKTOP-R6DHVIO\Otmars <ootmar1998@gmail.com>
2023-11-17 15:15:53 -0400

    reportes terminado

--- src/app/reportes/reportes.component.ts
+++ src/app/reportes/reportes.component.ts
@@ -1,37 +1,114 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import jsPDF from 'jspdf';
 import autoTable from 'jspdf-autotable';
+import { ReportesService } from './reportes.service';
 @Component({
   selector: 'app-reportes',
   templateUrl: './reportes.component.html',
   styleUrls: ['./reportes.component.css'],
 })
-export class ReportesComponent {
-  
-  generarPdf() {
-    const doc = new jsPDF({
-      format: 'letter',
+export class ReportesComponent implements OnInit {
+  materias: any;
+  constructor(private reportesService: ReportesService) {}
+  @Input() id: string;
+  ngOnInit(): void {
+    this.reportesService.getMarteria(this.id).subscribe((res) => {
+      this.materias = res;
+      console.log(this.materias);
     });
-    doc.addImage('../../assets/images/encabezado.png', 'PNG', 0, -10, 210, 55);
-    doc.setFont('helvetica', 'bold');
-    doc.text('REPORTE DE MATERIA INSCRITAS', 70, 35);
-    doc.text('REPORTE DE MATERIA INSCRITAS', 70, 35);
-    // doc.autoPrint({ variant: 'non-conform' });
-    
-    autoTable(doc, {
-      theme:'grid',
-      headStyles:{fillColor:[0, 0, 0]},
-      bodyStyles:{font:'helvetica'},
-      startY:40,
-      tableWidth:'wrap',
-      head: [['Name', 'Email', 'Country']],
-      body: [
-        ['David', 'david@example.com', 'Sweden'],
-        ['Castille', 'castille@example.com', 'Spain'],
-        // ...
-      ],
+  }
+
+  generarPdf(id: number) {
+    console.log(id);
+    let datos: any;
+    this.reportesService.getCalificaciones(id).subscribe((res) => {
+      datos = res[0];
+      console.log(datos);
+      var done: any = [];
+      datos.inscripcion.forEach(function (object: any) {
+        done.push([
+          object.id,
+          object.estudiante.iduser.nombres,
+          object.estudiante.iduser.apellidoPaterno,
+          object.estudiante.iduser.apellidoMaterno,
+          object.estudiante.iduser.ci,
+        ]);
+      });
+
+      const doc = new jsPDF({
+        format: 'letter',
+      });
+      doc.addImage(
+        '../../assets/images/encabezado.png',
+        'PNG',
+        0,
+        -10,
+        210,
+        55
+      );
+      doc.setFont('helvetica', 'bold');
+      doc.text('REPORTE DE CALIFICACION', 70, 35);
+      // doc.autoPrint({ variant: 'non-conform' });
+
+      autoTable(doc, {
+        theme: 'grid',
+        headStyles: {
+          fillColor: [0, 0, 0],
+          font: 'helvetica',
+          fontSize: 8,
+          halign: 'center',
+          
+        },
+        bodyStyles: { font: 'helvetica', fontSize: 8, cellWidth: 'wrap' },
+        columnStyles: {
+          id: { cellWidth: 15 },
+          nom: { cellWidth: 28 },
+          pat: { cellWidth: 25 },
+          mat: { cellWidth: 25 },
+          ci: { cellWidth: 18 },
+          nota: { },
+        },
+        columns: [
+          { header: 'id', dataKey: 'id' },
+          { header: 'Nombres', dataKey: 'nom' },
+          { header: 'Apellido Paterno', dataKey: 'pat' },
+          { header: 'Apellido Materno', dataKey: 'mat' },
+          { header: 'ci', dataKey: 'ci' },
+          { header: 'Nota', dataKey: 'nota' },
+          { header: 'Nota', dataKey: 'nota' },
+          { header: 'Nota', dataKey: 'nota' },
+          { header: 'Nota', dataKey: 'nota' },
+          { header: 'Nota', dataKey: 'nota' },
+          { header: 'Nota', dataKey: 'nota' },
+          { header: 'Nota', dataKey: 'nota' },
+          { header: 'Nota', dataKey: 'nota' },
+          { header: 'Nota', dataKey: 'nota' },
+          { header: 'Nota', dataKey: 'nota' },
+          { header: 'Nota', dataKey: 'nota' },
+          { header: 'Nota', dataKey: 'nota' },
+          { header: 'Nota', dataKey: 'nota' },
+          { header: 'Nota', dataKey: 'nota' },
+          { header: 'Nota', dataKey: 'nota' },
+          { header: 'Nota', dataKey: 'nota' },
+          { header: 'Nota', dataKey: 'nota' },
+          { header: 'Nota', dataKey: 'nota' },
+          { header: 'Nota', dataKey: 'nota' },
+          { header: 'Nota', dataKey: 'nota' },
+        ],
+        startY: 45,
+        tableWidth: 'auto',
+        head: [
+          [
+            'id',
+            'Nombres',
+            'Apellido Paterno',
+            'Apellido Materno',
+            'C.I.',
+          ],
+        ],
+        body: done,
+      });
+      doc.output('pdfobjectnewwindow');
     });
-    doc.output('pdfobjectnewwindow');
-    // doc.save()
   }
 }
